86670f8f7f0cc2bd4bd47aca64ddf39ef76a0b4f
SamPoyigi <sam@sampoyigi.com>
2018-10-09 17:25:16 +0100

    Fix migration issue caused by renamed namespace

--- database/migrations/2018_02_12_000300_create_online_tracker_table.php
+++ database/migrations/2018_02_12_000300_create_online_tracker_table.php
@@ -15,30 +15,9 @@ class CreateOnlineTrackerTable extends Migration
             OR !Schema::hasTable('customers_online'))
             return;
 
-        Schema::rename('customers_online', 'igniter_onlinetracker_tracker');
+        $this->createTrackerTable();
 
-        Schema::table('igniter_onlinetracker_tracker', function (Blueprint $table) {
-            $table->bigIncrements('activity_id')->change();
-            $table->string('session_id')->nullable();
-            $table->integer('geoip_id')->nullable();
-            $table->string('platform')->nullable();
-            $table->text('headers')->nullable();
-            $table->text('query')->nullable();
-            $table->dropColumn('date_added');
-            $table->timestamps();
-        });
-
-        Schema::create('igniter_onlinetracker_geoip', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->bigIncrements('id');
-            $table->double('latitude')->nullable()->index();
-            $table->double('longitude')->nullable()->index();
-            $table->string('region', 6)->nullable();
-            $table->string('city', 50)->nullable()->index();
-            $table->string('postal_code', 20)->nullable();
-            $table->string('country_iso_code_2', 2)->nullable()->index();
-            $table->timestamps();
-        });
+        $this->createGeoIpTable();
     }
 
     public function down()
@@ -55,5 +34,49 @@ class CreateOnlineTrackerTable extends Migration
         Schema::rename('igniter_onlinetracker_tracker', 'customers_online');
 
         Schema::dropIfExists('igniter_onlinetracker_geoip');
+    }
+
+    protected function createTrackerTable()
+    {
+        if (Schema::hasTable('customers_online'))
+            Schema::rename('customers_online', 'igniter_onlinetracker_tracker');
+
+        if (Schema::hasTable('igniterlab_onlinetracker_tracker'))
+            Schema::rename('igniterlab_onlinetracker_tracker', 'igniter_onlinetracker_tracker');
+
+        if (Schema::hasTable('igniter_onlinetracker_tracker'))
+            return;
+
+        Schema::table('igniter_onlinetracker_tracker', function (Blueprint $table) {
+            $table->bigIncrements('activity_id')->change();
+            $table->string('session_id')->nullable();
+            $table->integer('geoip_id')->nullable();
+            $table->string('platform')->nullable();
+            $table->text('headers')->nullable();
+            $table->text('query')->nullable();
+            $table->dropColumn('date_added');
+            $table->timestamps();
+        });
+    }
+
+    protected function createGeoIpTable()
+    {
+        if (Schema::hasTable('igniterlab_onlinetracker_geoip'))
+            Schema::rename('igniterlab_onlinetracker_geoip', 'igniter_onlinetracker_geoip');
+
+        if (Schema::hasTable('igniter_onlinetracker_geoip'))
+            return;
+
+        Schema::create('igniter_onlinetracker_geoip', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->double('latitude')->nullable()->index();
+            $table->double('longitude')->nullable()->index();
+            $table->string('region', 6)->nullable();
+            $table->string('city', 50)->nullable()->index();
+            $table->string('postal_code', 20)->nullable();
+            $table->string('country_iso_code_2', 2)->nullable()->index();
+            $table->timestamps();
+        });
     }
 }
